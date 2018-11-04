26fcfbbc1745c28c0d1a4ef7a368ce1be4226f0b
Nicolas Kao Sartor <nkao552@gmail.com>
2018-11-04 13:27:43 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Chrome extension base with parcel and react.
+Hot reload working.
+
+ATTENTION: manbifest.json was manually places inside dist folder
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+	return(
+		<div>
+			<h1>Hello Parcel refresh again</h1>
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
